0cef0cbd7a2004807db15025b90e2fb39baced86
shangguan999 <shangguanhaostudy@gmail.com>
2025-06-28 22:01:49 +0800

    Fix: re-add foodly_restaurant as normal folder

---
+++ foodly_restaurant/lib/models/environment.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:foodly_restaurant/models/environment.dart';
+
+class Environment {
+  static String get fileName {
+    if (kReleaseMode) {
+      return '.env.production';
+    }
+
+      return '.env.development';
+  }
+
+  static String get googleApiKey {
+    return dotenv.env['GOOGLE_API_KEY'] ?? 'API_KEY not found';
+  }
+
+   static String get googleApiKey2 {
+    return dotenv.env['GOOGLE_API_KEY2'] ?? 'API_KEY not found';
+  }
+
+    static String get appBaseUrl {
+    return dotenv.env['API_BASE_URL'] ?? 'API_BASE_URL not found';
+  }
+
+   static String get paymentUrl {
+    return dotenv.env['PAYMENT_URL'] ?? 'PAYMENT_BASE_URL not found';
+  }
+}
